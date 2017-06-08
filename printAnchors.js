5661aa2dc67c354cca7511c56459e5f284d3f86c
Sean Kelley <s.kelley27@gmail.com>
2017-06-08 13:09:11 -0500

    add file to print out found anchors from selected sites to the console

---
+++ printAnchors.js
@@ -0,0 +1,22 @@
+var siteFileUtility = require('./siteFileUtility'),
+    cheerio = require('cheerio'),
+    request = require('sync-request');
+
+function printSiteAnchors(sites){
+    for(var i = 0; i < sites.length; i++){
+        console.log(sites[i].name);
+        for(var j = 0; j < sites[i].pages.length; j++){
+            console.log("# " + sites[i].pages[j]);
+            var res = request('GET', sites[i].pages[j]);
+            var pageHtml = res.getBody();
+            var $ = cheerio.load(pageHtml);
+            var anchorList = $('a');
+            for(var k = 0; k < anchorList.length; k++){
+                console.log("-   " + anchorList[k].attribs.href);
+            }
+        }
+    }
+}
+
+var sites = siteFileUtility.getSites();
+printSiteAnchors(sites);
