a0a73505a2532586f735f4a22bd413df7a16de45
Sean Kelley <s.kelley27@gmail.com>
2017-06-08 16:22:23 -0500

    check for exceptions when geting websites

--- printAnchors.js
+++ printAnchors.js
@@ -7,12 +7,16 @@ function printSiteAnchors(sites){
         console.log(sites[i].name);
         for(var j = 0; j < sites[i].pages.length; j++){
             console.log("# " + sites[i].pages[j]);
-            var res = request('GET', sites[i].pages[j]);
-            var pageHtml = res.getBody();
-            var $ = cheerio.load(pageHtml);
-            var anchorList = $('a');
-            for(var k = 0; k < anchorList.length; k++){
-                console.log("-   " + anchorList[k].attribs.href);
+            try{
+                var res = request('GET', sites[i].pages[j]);
+                var pageHtml = res.getBody();
+                var $ = cheerio.load(pageHtml);
+                var anchorList = $('a');
+                for(var k = 0; k < anchorList.length; k++){
+                    console.log("-   " + anchorList[k].attribs.href);
+                }
+            } catch(e) {
+                console.log("-   Exception encountered getting page.");
             }
         }
     }
