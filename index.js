742f7591ee698a487043f47b4061f359e1b937ab
Sean Kelley <s.kelley27@gmail.com>
2017-06-09 14:40:17 -0500

    finish with processing add nodemailer dep

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ function processSites(sites){
 	for(var i = 0; i < sites.length; i++){
 		var badAnchors = [];
 		for(var j = 0; j < sites[i].pages.length; j++){
-			badAnchors.push(processSite(sites[i].pages[j], sites[i].whitelist));
-			// return map or object of site to error anchors
+			badAnchors = badAnchors.concat(processSite(sites[i].pages[j], sites[i].whitelist));
 		}
 		if(badAnchors.length > 0){
 			badLinks.push({
@@ -27,14 +26,14 @@ function processSite(url, whitelist){
 	    var res = request('GET', url);
 	    var pageHtml = res.getBody();
 	    var $ = cheerio.load(pageHtml);
-	    var anchorList = $('a');
-	    for(var i = 0; i < anchorList.length; i++){
-	    	if(!isLinkOkay(anchorList[i].attribs.href, whitelist)){
-	    		badLinks.push(anchorList[i].attribs.href);
+	    $('a').each(function(i, element){
+	    	var linkOkay = isLinkOkay($(this).attr('href'), whitelist);
+	    	if(!linkOkay){
+	    		badLinks.push($(this).attr('href'));
 	    	}
-	    }
+		});
 	} catch(e) {
-	      console.log("Exception encountered getting page: " + url);
+	      console.log("Exception encountered processing page: " + url);
 	}
 	return badLinks;
 }
@@ -45,15 +44,43 @@ function isLinkOkay(linkHref, whitelist){
 		if(pattern.test(linkHref)){
 			return true;
 		}
-	}
+	};
 	return false;
 }
 
-function processErrors(errors){
-	//send notifications to notification contact person
+function processNotifications(badLinks){
+	var notifiedParties = badLinks.reduce(function(acc,val){
+		if(val.badLinks.length > 0 && !acc.includes(val.notify)){
+			return acc.concat(val.notify);
+		}else{
+			return acc;
+		}
+	},[]);
+
+	notifiedParties.forEach(function(party){
+		var badLinksForParty = badLinks.reduce(function(acc,val){
+			if(val.notify === party && val.badLinks.length){
+				return acc.concat(val);
+			}else{
+				return acc;
+			}
+		},[]);
+		message = "Attention! You have bad links on the following watched sites: <br/>";
+		for(var i = 0; i < badLinksForParty.length; i++){
+			message += badLinksForParty[i].name + "<br/>";
+			for(var j = 0; j < badLinksForParty[i].badLinks.length; j++){
+				message += "-	" + badLinksForParty[i].badLinks[j] + "<br/>";
+			}
+		}
+		sendNotification(party, message);
+	});
+}
+
+function notify(email, message){
+
 }
 
 var sites = siteFileUtility.getSites();
 var badLinks = processSites(sites);
-// processErrors(errors);
+// processNotifications(badLinks);
 console.log(JSON.stringify(badLinks));
