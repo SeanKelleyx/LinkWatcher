79db850f38c41cfa553933eb72219e54354777e0
sean kelley <kelley@KelleyServer.kelley>
2017-06-11 17:27:37 -0500

    get full file path in case of runnign from another directory

--- siteFileUtility.js
+++ siteFileUtility.js
@@ -1,7 +1,9 @@
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path'),
+    appDir = path.dirname(require.main.filename);;
 
 var _getSites = function(){
-    var sitesArray = JSON.parse(fs.readFileSync('sites.json'));
+    var sitesArray = JSON.parse(fs.readFileSync(appDir + '/sites.json'));
     var myArgs = process.argv.slice(2);
     if(myArgs.length>0){
         sitesArray =  sitesArray.filter(function(site){
