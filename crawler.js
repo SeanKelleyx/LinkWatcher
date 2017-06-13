28c5b2d42bd4b1af0191bb641cf9b2e52da4d468
Sean Kelley <s.kelley27@gmail.com>
2017-06-12 22:26:40 -0500

    modify crawler to return instance of self including list of external links

--- crawler.js
+++ crawler.js
@@ -3,12 +3,11 @@ var cheerio = require('cheerio');
 var URL = require('url-parse');
 
 class Crawler {
-  constructor(startUrl){
+  constructor(startUrl, doneProcessing){
     this.START_URL = startUrl;
-    this.MAX_PAGES_TO_VISIT= 10;
-    this.SEARCH_WORD = "stemming";
+    this.doneProcessing = doneProcessing;
     this.pagesVisited = {};
-    this.numPagesVisited = 0;
+    this.externalLinksFound = [];
     this.pagesToVisit = [];
     this.url = new URL(this.START_URL);
     this.baseUrl = this.url.protocol + "//" + this.url.hostname;
@@ -16,71 +15,64 @@ class Crawler {
     var self = this;
     
     this.crawl = function(){
-      if(self.numPagesVisited >= self.MAX_PAGES_TO_VISIT) {
-        console.log("Reached max limit of number of pages to visit.");
-        return;
-      }
-      //assumes there are always more to visit
-      var nextPage = self.pagesToVisit.pop();
-      if (nextPage in self.pagesVisited) {
-        // We've already visited this page, so repeat the crawl
-        self.crawl();
-      } else {
-        // New page we haven't visited
-        console.log(nextPage);
-        self.visitPage(nextPage, self.crawl);
+      if(self.pagesToVisit.length > 0){
+        var nextPage = self.pagesToVisit.pop();
+        if (nextPage in self.pagesVisited) {
+          self.crawl();
+        } else {
+          self.visitPage(nextPage, self.crawl);
+        }
+      }else{
+        self.doneProcessing(self);
       }
     }
 
     this.visitPage = function(url, callback) {
       console.log(url);
-      // Add page to our set
       self.pagesVisited[url] = true;
-      self.numPagesVisited++;
-
-      // Make the request
-      console.log("Visiting page " + url);
       request(url, function(error, response, body) {
         if(error){
           console.log("error: " + error);
-        }
-        // Check status code (200 is HTTP OK)
-        if(response && response.statusCode !== 200) {
-          console.log("Status code: " + response.statusCode);
           callback();
           return;
         }
-        // Parse the document body
-        var $ = cheerio.load(body);
-        console.log(typeof self.searchForWord);
-        var isWordFound = self.searchForWord($, self.SEARCH_WORD);
-        if(isWordFound) {
-          console.log('Word ' + self.SEARCH_WORD + ' found at page ' + url);
-        } else {
-          self.collectInternalLinks($);
-          // In this short program, our callback is just calling crawl()
+        if(response && response.statusCode !== 200) {
           callback();
+          return;
         }
+        var $ = cheerio.load(body);
+        self.collectLinks($);
+        callback();
       });
     }
 
-    this.searchForWord = function($, word) {
-      var bodyText = $('html > body').text().toLowerCase();
-      return(bodyText.indexOf(word.toLowerCase()) !== -1);
-    }
-
-    this.collectInternalLinks = function($) {
-      var relativeLinks = $("a[href^='/']");
-      console.log("Found " + relativeLinks.length + " relative links on page");
-      relativeLinks.each(function() {
-        self.pagesToVisit.push(self.baseUrl + $(this).attr('href'));
+    this.collectLinks = function($) {
+      var allLinks = $("a");
+      allLinks.each(function() {
+        var href = $(this).attr('href');
+        if(href){
+          var absoluteRegExp = new RegExp('^(?:[a-z]+:)?//', 'i');
+          if(absoluteRegExp.test(href) && href.indexOf(self.url.hostname) != 7 && href.indexOf(self.url.hostname) != 8){
+            if(!self.externalLinksFound.includes(href)){
+              self.externalLinksFound.push(href);
+            }
+          }else{
+            if(absoluteRegExp.test(href)){
+              self.pagesToVisit.push(href);
+            }else if(href[0] === "/"){
+              self.pagesToVisit.push(self.baseUrl + href);
+            }else{
+              self.pagesToVisit.push(self.baseUrl + "/" + href);
+            }
+          }
+        }
       });
     }
   }
 }
 
-function _runCrawler(url){
-  var crawler = new Crawler(url);
+function _runCrawler(url, callback){
+  var crawler = new Crawler(url, callback);
   crawler.crawl();
 }
 
